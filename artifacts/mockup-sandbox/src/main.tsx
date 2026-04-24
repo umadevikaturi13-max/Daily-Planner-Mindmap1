b360b01bffb5ed5b200cea768a6b906d33db4236
umadevikaturi13-max <umadevikaturi13@gmail.com>
2026-04-25 00:05:42 +0530

    Update main.tsx

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from "react-dom/client";
+/* import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+*/
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
